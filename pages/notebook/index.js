6393817bbae913d996d4cae21ab2bd7065c7721b
Forkan <forkanalam295@gmail.com>
2023-01-07 19:52:46 +0600

    add editor

--- pages/notebook/index.js
+++ pages/notebook/index.js
@@ -1,69 +1,52 @@
-import React from 'react';
-import Layout from '../../src/includes/noteapp/layout'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import ReactQuill, { Quill, editor } from "react-quill";
+import Layout from "../../src/includes/noteapp/layout";
+import "react-quill/dist/quill.snow.css";
 
 const Index = () => {
-    const onDragEndHandle = () => {
-        alert('asdf');
-    }
+  const textInput = useRef(null);
+  const textEditor = useRef(null);
+  const [isEditorShow, setIsEditorShow] = useState(false);
 
-    const arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    return (
-        <Layout>
-            <div className="py-6">
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                    <DragDropContext onDragEnd={onDragEndHandle}>
-                        <Droppable droppableId="droppable">
-                            {(provided) => (
-                                <div
-                                    ref={provided.innerRef}
+  useEffect(() => {
+    // textEditor.current.style.display = "none";
+    console.log(textEditor.current);
 
-                                    {...provided.droppableProps}
+    document.addEventListener(["keydown", "click"], function (e) {
+      if (textInput.current) {
+        setIsEditorShow(true);
+      }
+    });
+  });
 
-                                    className="flex flex-col w-full"
-
-                                    data-testid="droppable"
-
-                                    style={{
-                                        border: '1px solid #e0e0e0',
-                                        borderRadius: '4px',
-                                        padding: '20px',
-                                        width: '100%',
-                                        height: '100%',
-                                        overflow: 'auto',
-                                        position: 'relative'
-                                    }}
-                                >
-                                    {arrr.map(item =>
-                                        <Draggable draggableId={'id_' + item} index={item}>
-                                            {(provideds) => (
-                                                <h1
-                                                    ref={provideds.innerRef}
-                                                    {...provideds.draggableProps}
-                                                    {...provideds.dragHandleProps}
-                                                    className="text-2xl font-semibold text-gray-900">Dashboard
-                                                </h1>
-                                            )}
-                                        </Draggable>
-                                    )}
-                                    {provided.placeholder}
-                                </div>
-
-                            )}
-                        </Droppable>
-                    </DragDropContext>
-
-                </div>
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                    {/* Replace with your content */}
-                    <div className="py-4">
-                        <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-                    </div>
-                    {/* /End replace */}
-                </div>
-            </div>
-        </Layout>
-    );
-}
+  return (
+    <Layout>
+      <div className="py-6">
+        <div class="flex justify-center ..."></div>
+        <div class="flex justify-center ...">
+          {isEditorShow ? (
+            <ReactQuill ref={textEditor} />
+          ) : (
+            <input
+              ref={textInput}
+              type="text"
+              name=""
+              id="note"
+              placeholder="Take a note..."
+            />
+          )}
+        </div>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+          {/* Replace with your content */}
+          <div className="py-4">
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+          </div>
+          {/* /End replace */}
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default Index;
