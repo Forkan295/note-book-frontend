62b9ce4c09d5d754f317e63da0ea95844df03ed7
Forkan <forkanalam295@gmail.com>
2023-01-07 20:02:50 +0600

    fixed react quill render document on server side error

--- pages/notebook/index.js
+++ pages/notebook/index.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { useEffect, useState, useRef } from "react";
-import ReactQuill, { Quill, editor } from "react-quill";
+
+import dynamic from "next/dynamic";
+const ReactQuill = dynamic(import("react-quill"), { ssr: false });
 import Layout from "../../src/includes/noteapp/layout";
 import "react-quill/dist/quill.snow.css";
 
 const Index = () => {
   const textInput = useRef(null);
-  const textEditor = useRef(null);
   const [isEditorShow, setIsEditorShow] = useState(false);
 
   useEffect(() => {
-    // textEditor.current.style.display = "none";
-    console.log(textEditor.current);
-
-    document.addEventListener(["keydown", "click"], function (e) {
+    document.addEventListener("keydown", function (e) {
       if (textInput.current) {
         setIsEditorShow(true);
       }
     });
   });
+
+  const handleInputClick = () => {
+    setIsEditorShow(true);
+  };
 
   return (
     <Layout>
@@ -26,10 +28,11 @@ const Index = () => {
         <div class="flex justify-center ..."></div>
         <div class="flex justify-center ...">
           {isEditorShow ? (
-            <ReactQuill ref={textEditor} />
+            <ReactQuill />
           ) : (
             <input
               ref={textInput}
+              onClick={handleInputClick}
               type="text"
               name=""
               id="note"
