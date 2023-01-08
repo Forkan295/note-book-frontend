edb4d38997f53771c421064e3a2869396c0bb81f
forkan <forkanalam295@gmail.com>
2023-01-08 23:14:18 +0600

    styling editor

--- pages/notebook/index.js
+++ pages/notebook/index.js
@@ -4,8 +4,13 @@ import { useEffect, useState, useRef } from "react";
 import dynamic from "next/dynamic";
 const ReactQuill = dynamic(import("react-quill"), { ssr: false });
 import Layout from "../../src/includes/noteapp/layout";
+import styles from './css/note.module.css'
 import "react-quill/dist/quill.snow.css";
 
+
+function classNames(...styles) {
+    return styles.filter(Boolean).join(' ')
+  }
 const Index = () => {
   const textInput = useRef(null);
   const [isEditorShow, setIsEditorShow] = useState(false);
@@ -30,19 +35,22 @@ const Index = () => {
           {isEditorShow ? (
             <ReactQuill />
           ) : (
-            <input
+           <div className={classNames(styles.inputContainer)}>
+             <input
               ref={textInput}
               onClick={handleInputClick}
               type="text"
               name=""
               id="note"
+              className={classNames(styles.inputStyle)}
               placeholder="Take a note..."
             />
+           </div>
           )}
         </div>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
           {/* Replace with your content */}
-          <div className="py-4">
+          <div className="py-20">
             <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
           </div>
           {/* /End replace */}
